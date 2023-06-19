7da4959c297d0f9258efb045e722ea7f33fbb5bb
shanmukha <panapanashanmukha6@gmail.com>
2023-06-19 12:29:50 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MypageComponent } from './mypage/mypage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MypageComponent,
+    NavbarComponent,
+    FooterComponent,
+  
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mypage/mypage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mypage',
+  templateUrl: './mypage.component.html',
+  styleUrls: ['./mypage.component.css']
+})
+export class MypageComponent {
+
+}
